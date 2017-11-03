17f132e581f158ebc9e50b8d24c9cbc0ba102059
mettewirak <mettewirak@hotmail.com>
2017-11-03 16:52:09 +0100

    Started on ising-functions.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+double** create_matrix(int dim, int n);
+void delete_matrix(int dim, int n, double** state);
+double ising_first(double** state, int dim, int n);
+void ising_diff(double** state, double* energy);
+
 int main()
 {
 	// MÅ TA INN
@@ -40,4 +45,97 @@ int main()
 
 
 	return 0;
+}
+
+double** create_matrix(int dim, int n){
+
+    double **state;
+        state = new double*[dim];
+
+        for(int i=0; i<total_planets; i++){
+            state[i] = new double[n];
+        }
+
+    return state;
+}
+
+void delete_matrix(int dim, int n, double** state){
+
+    for(int i=0; i<n; i++){
+        delete [] state[i];
+    }
+    delete [] state;
+}
+
+double ising_first(double **state, int dim, int n){ // MÅ TILPASSES ANDRE DIMENSJONER ENN 2
+
+    double J; // Coupling constant
+    double energy = 0.0;
+    double temp = 0.0;
+
+    for(int x=0; x<n; x++){
+
+        for(int y=0; y<n; y++){
+
+            // KANT VENSTRE
+            if(x == 0)
+                temp += state[n-1][y];
+            else
+                temp += state[x-1][y];
+
+            // KANT OPPE
+            if(y == 0)
+                temp += state[x][n-1];
+            else
+                temp += state[x][y-1];
+
+            // KANT HØYRE
+            if(x == (n-1))
+                temp += state[0][y];
+            else
+                temp += state[x+1][y];
+
+            // KANT NEDE
+            if( y == (n-1))
+                temp += state[x][0];
+            else
+                temp += state[x][y+1];
+
+            energy += state[x][y]*temp;
+            temp = 0.0;
+        }
+    }
+    return (-J*energy);
+}
+
+void ising_diff(double **state, double *energy){
+
+    double J; // Coupling constant
+    int temp = 0.0;
+
+    // KANT VENSTRE
+    if(x == 0)
+        temp += state[n-1][y];
+    else
+        temp += state[x-1][y];
+
+    // KANT OPPE
+    if(y == 0)
+        temp += state[x][n-1];
+    else
+        temp += state[x][y-1];
+
+    // KANT HØYRE
+    if(x == (n-1))
+        temp += state[0][y];
+    else
+        temp += state[x+1][y];
+
+    // KANT NEDE
+    if( y == (n-1))
+        temp += state[x][0];
+    else
+        temp += state[x][y+1];
+
+    *energy = (2*J*state[x][y]*temp);
 }
