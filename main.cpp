7728684164a1ea6a5821bc376f34a350849ea018
mettewirak <mettewirak@hotmail.com>
2017-11-06 23:21:26 +0100

    Somewhat functioning. RNG must be replaced with a better one, and results must be saved to file.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,10 @@ int** create_matrix_2(int n);
 void delete_matrix_2(int n, int** state);
 void print_matrix_to_screen(int n, int** state);
 void initialize(int** state, double temperature, int& E, int& M, int n);
-//void ising_diff(int** state, int* energy);
+int periodic(int position, int N, int jump);
+void Metropolis(int N, double random_number, int **spins, int& E, int& M, double *w);
+double r2();
+
 
 int main()
 {
@@ -20,22 +23,27 @@ int main()
     //int dimension = 2;
     int N = 2;
     int** spins;
-    //int MC_iterations = pow(10, 4);
-    double final_temp = 4, initial_temp = 1;
-    double temp_step = (final_temp-initial_temp)/pow(10,0);
+    double final_temp = 3, initial_temp = 1;
+    double temp_step = (final_temp-initial_temp)/pow(10,1);
     int energy = 0, magnetization = 0;
 
-	// Skal gjøres for mange temperaturer. Må ha en for-løkke for dette ytterst.
+
+    // Skal gjøres for mange temperaturer. Må ha en for-løkke for dette ytterst.
 
     for(int temperature = initial_temp; temperature < final_temp; temperature += temp_step){
+
+        // Precalculate probabilities according to the boltzmann distribution for the different energy changes
+        double w[17];
+        for(int de = -8; de < 8; de ++)
+            w[de] = 0.0;
+        for(int de = -8; de < 8; de +=4)
+            w[de] = exp(-de/temperature);
 
     // Lagre verdier i en peke-matrise innenfor temperaturløkka? Og så skrive til til for hver temperatur?
 
         spins = create_matrix_2(N);
 
-        print_matrix_to_screen(N, spins);
         initialize(spins, temperature, energy, magnetization, N);
-        print_matrix_to_screen(N, spins);
 
         cout << "The energy initial energy is " << energy << ", and the magnetization is " << magnetization << ".\n";
 
@@ -46,20 +54,12 @@ int main()
 	// Dette beskriver da en spesifikk partikkel som evt. skal endre spin.
 	// Monte Carlo kjøres i en for-løkke ca 10^6 ganger.  
 
-        // int random_spin = 1;
+        for(int i=0; i < pow(10, 1); i++){
 
-	// ISING
-	// Regner ut energien til hele systemet slik det er nå (dette kan sannsynligvis lagres på), og energien hvis partikkelen funnet
-	// i Monte Carlo skifter spinn. Dette tas med inn i Metropolis. Skal bruke periodiske grensebetingelser. 
-	// Bør være en egen funksjon.
+            Metropolis(N, r2(), spins, energy, magnetization, w);
+            print_matrix_to_screen(N, spins);
 
-
-	// METROPOLIS
-	// Regner ut sannsynligheten for at endringen blir gjennomført, gitt av W = AT. Må ha en grense for hvor høy sannsynligheten skal
-	// være for at endringen skal gjøres?
-
-
-
+        }
 	// RESULTATER
 	// Gi ut gjennomsnittsenergi, gjennomsnittsmagnetisering, varmekapasitet og suseptebilitet.
     }
@@ -68,6 +68,7 @@ int main()
 
 	return 0;
 }
+
 
 int** create_matrix_2(int n){
 
@@ -87,6 +88,7 @@ int** create_matrix_2(int n){
     return state;
 }
 
+
 void delete_matrix_2(int n, int** state){
 
     for(int i=0; i<n; i++){
@@ -94,6 +96,7 @@ void delete_matrix_2(int n, int** state){
     }
     delete [] state;
 }
+
 
 void print_matrix_to_screen(int n, int **state){
 
@@ -107,6 +110,7 @@ void print_matrix_to_screen(int n, int **state){
     }
 }
 
+
 void initialize(int **state, double temperature, int& E, int& M, int n){ // Uses periodic boundary conditions.
 
     int J = 1; // Coupling constant
@@ -115,12 +119,13 @@ void initialize(int **state, double temperature, int& E, int& M, int n){ // Uses
     // Initialiserer til at alle spin peker opp hvis temperaturen er lav. Skal ellers fortsette som det var i temperaturen før.
     for(int y = 0; y < n; y++){
         for(int x = 0; x < n; x++){
-            if(temperature<1.5)
+            if(temperature < 1.5)
                 state[x][y] = 1;
         }
     }
 
     E = M = 0;
+
     for(int y=0; y<n; y++){
         for(int x=0; x<n; x++){
 
@@ -156,34 +161,49 @@ void initialize(int **state, double temperature, int& E, int& M, int n){ // Uses
     }
 }
 
-/*void ising_diff(int **state, int *energy){
 
-    int J = 1; // Coupling constant
-    int temp = 0.0;
+periodic(int position, int N, int jump){ // Mulig å gjøre dette uten if? Det er tydeligvis dårlig
 
-    // KANT VENSTRE
-    if(x == 0)
-        temp += state[n-1][y];
-    else
-        temp += state[x-1][y];
+    if((position+jump) < 0){
+        return (N-1);
+    }
+    else if((position+jump) > (N-1)){
+        return 0;
+    }
+    else{
+        return (position + jump);
+    }
+}
 
-    // KANT OPPE
-    if(y == 0)
-        temp += state[x][n-1];
-    else
-        temp += state[x][y-1];
 
-    // KANT HØYRE
-    if(x == (n-1))
-        temp += state[0][y];
-    else
-        temp += state[x+1][y];
+void Metropolis(int N, double random_number, int **spins, int& E, int& M, double *w){
 
-    // KANT NEDE
-    if( y == (n-1))
-        temp += state[x][0];
-    else
-        temp += state[x][y+1];
 
-    *energy = (2*J*state[x][y]*temp);
-}*/
+    // METROPOLIS
+    // Regner ut sannsynligheten for at endringen blir gjennomført, gitt av W = AT. Må ha en grense for hvor høy sannsynligheten skal
+    // være for at endringen skal gjøres?
+
+    int deltaE = 0;
+    int random_spin_x = rand()%2;
+    int random_spin_y = rand()%2;
+
+    deltaE = 2*spins[random_spin_x][random_spin_y]*
+         (spins[random_spin_x][periodic(random_spin_y, N, 1)] + spins[random_spin_x][periodic(random_spin_y, N, -1)] +
+         spins[periodic(random_spin_x, N, 1)][random_spin_y] + spins[periodic(random_spin_x, N, -1)][random_spin_y]);
+
+    cout << "Spin to be flipped: " << random_spin_x << ", " << random_spin_y << endl;
+    cout << "DeltaE: " << deltaE << endl;
+    cout << "Compare " << random_number << " and " << w[deltaE+8] << endl;
+
+    if(random_number > w[deltaE+8]){
+         spins[random_spin_x][random_spin_y] *= -1;
+         E += deltaE;
+         M += 2*spins[random_spin_x][random_spin_y];
+    }
+}
+
+
+double r2()
+{
+    return (double)rand() / (double)RAND_MAX ;
+}
