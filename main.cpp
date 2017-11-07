0a1aaf0cff52890c478ea95d5402828832171cb2
mettewirak <mettewirak@hotmail.com>
2017-11-07 14:16:00 +0100

    Mostly seems to be working.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 #include <cmath>
 using namespace std;
+ofstream ofile;
+ofstream ofile2;
+
 
 int** create_matrix_2(int n);
 void delete_matrix_2(int n, int** state);
@@ -9,44 +14,39 @@ void initialize(int** state, double temperature, int& E, int& M, int n);
 int periodic(int position, int N, int jump);
 void Metropolis(int N, double random_number, int **spins, int& E, int& M, double *w);
 double r2();
+void output(int N, int mc_cycles, double temperature, double* expectation_values);
 
 
 int main()
 {
-	// MÅ TA INN
-	// - Dimensjoner
-	// - Antall partikler i hver dimensjon
-	// - Hvor mange ganger Monte Carlo skal kjøres
-	// - En grense for hvor høy sannsynligheten skal være før noe gjennomføres?
-	// - Temperaturintervallet funksjonen skal kjøres over.
-
-    //int dimension = 2;
-    int N = 2;
+    int N = 20;
     int** spins;
-    double final_temp = 3, initial_temp = 1;
-    double temp_step = (final_temp-initial_temp)/pow(10,1);
-    int energy = 0, magnetization = 0;
+    spins = create_matrix_2(N);
 
+    double final_temp = 4;
+    double initial_temp = 1;
+    int mc_cycles = pow(10,5);
+    double temp_step = 0.50;
+    int energy = 0;
+    int magnetization = 0;
+    double expectation_values[5]; // E, E*E, M, M*M, |M|
+    double w[17];
 
-    // Skal gjøres for mange temperaturer. Må ha en for-løkke for dette ytterst.
+    srand(time(NULL));
 
-    for(int temperature = initial_temp; temperature < final_temp; temperature += temp_step){
+    ofile.open("Resultater.txt");
+    //ofile2.open("MC-utvikling.txt");
 
-        // Precalculate probabilities according to the boltzmann distribution for the different energy changes
-        double w[17];
-        for(int de = -8; de < 8; de ++)
-            w[de] = 0.0;
-        for(int de = -8; de < 8; de +=4)
-            w[de] = exp(-de/temperature);
+    for(double temperature = initial_temp; temperature <= final_temp; temperature += temp_step){
 
-    // Lagre verdier i en peke-matrise innenfor temperaturløkka? Og så skrive til til for hver temperatur?
-
-        spins = create_matrix_2(N);
+        for(int i=0; i<5; i++){
+            expectation_values[i] = 0.0;}
+        for(int de = -8; de <= 8; de ++){
+            w[de+8] = 0.0;}
+        for(int de = -8; de <= 8; de +=4){
+            w[de+8] = exp(-de/temperature);}
 
         initialize(spins, temperature, energy, magnetization, N);
-
-        cout << "The energy initial energy is " << energy << ", and the magnetization is " << magnetization << ".\n";
-
 
 	// MONTE CARLO
 	// Må lage en random number generator.
@@ -54,18 +54,30 @@ int main()
 	// Dette beskriver da en spesifikk partikkel som evt. skal endre spin.
 	// Monte Carlo kjøres i en for-løkke ca 10^6 ganger.  
 
-        for(int i=0; i < pow(10, 1); i++){
-
+        for(int cycles = 0; cycles < mc_cycles; cycles++){
             Metropolis(N, r2(), spins, energy, magnetization, w);
-            print_matrix_to_screen(N, spins);
+            expectation_values[0] += energy;
+            expectation_values[1] += energy*energy;
+            expectation_values[2] += magnetization;
+            expectation_values[3] += magnetization*magnetization;
+            expectation_values[4] += fabs(magnetization);
+
+            //ofile2 << setiosflags(ios::showpoint | ios::uppercase);
+            //ofile2 << setw(15) << setprecision(8) << expectation_values[0]/((double)(mc_cycles));
+            //ofile2 << setw(15) << setprecision(8) << expectation_values[3]/((double)(mc_cycles));
+            //ofile2 << endl;
 
         }
-	// RESULTATER
-	// Gi ut gjennomsnittsenergi, gjennomsnittsmagnetisering, varmekapasitet og suseptebilitet.
+
+        cout << "Temperature " << temperature << endl;
+
+        print_matrix_to_screen(N, spins);
+        output(N, mc_cycles, temperature, expectation_values);
     }
 
     delete_matrix_2(N, spins);
-
+    ofile.close();
+    //ofile2.close();
 	return 0;
 }
 
@@ -114,12 +126,12 @@ void print_matrix_to_screen(int n, int **state){
 void initialize(int **state, double temperature, int& E, int& M, int n){ // Uses periodic boundary conditions.
 
     int J = 1; // Coupling constant
-    int temp_energy = 0;
+    int sum = 0;
 
     // Initialiserer til at alle spin peker opp hvis temperaturen er lav. Skal ellers fortsette som det var i temperaturen før.
     for(int y = 0; y < n; y++){
         for(int x = 0; x < n; x++){
-            if(temperature < 1.5)
+            if(temperature < 1.3)
                 state[x][y] = 1;
         }
     }
@@ -129,32 +141,11 @@ void initialize(int **state, double temperature, int& E, int& M, int n){ // Uses
     for(int y=0; y<n; y++){
         for(int x=0; x<n; x++){
 
-            // KANT VENSTRE
-            if(x == 0)
-                temp_energy += state[n-1][y];
-            else
-                temp_energy += state[x-1][y];
+            sum = state[x][periodic(y, n, -1)] + state[x][periodic(y, n, +1)]
+                    + state[periodic(x, n, -1)][y] + state[periodic(x, n, +1)][y];
 
-            // KANT OPPE
-            if(y == 0)
-                temp_energy += state[x][n-1];
-            else
-                temp_energy += state[x][y-1];
-
-            // KANT HØYRE
-            if(x == (n-1))
-                temp_energy += state[0][y];
-            else
-                temp_energy += state[x+1][y];
-
-            // KANT NEDE
-            if( y == (n-1))
-                temp_energy += state[x][0];
-            else
-                temp_energy += state[x][y+1];
-
-            E += -J*state[x][y]*temp_energy;
-            temp_energy = 0;
+            E += -J*state[x][y]*sum;
+            sum = 0;
 
             M += state[x][y];
         }
@@ -178,27 +169,23 @@ periodic(int position, int N, int jump){ // Mulig å gjøre dette uten if? Det e
 
 void Metropolis(int N, double random_number, int **spins, int& E, int& M, double *w){
 
+    for(int x = 0; x<N; x++){
+        for(int y = 0; y<N; y++){
 
-    // METROPOLIS
-    // Regner ut sannsynligheten for at endringen blir gjennomført, gitt av W = AT. Må ha en grense for hvor høy sannsynligheten skal
-    // være for at endringen skal gjøres?
+            int deltaE = 0;
+            int random_spin_x = rand()%N;
+            int random_spin_y = rand()%N;
 
-    int deltaE = 0;
-    int random_spin_x = rand()%2;
-    int random_spin_y = rand()%2;
+            deltaE = 2*spins[random_spin_x][random_spin_y]*
+                (spins[random_spin_x][periodic(random_spin_y, N, 1)] + spins[random_spin_x][periodic(random_spin_y, N, -1)] +
+                spins[periodic(random_spin_x, N, 1)][random_spin_y] + spins[periodic(random_spin_x, N, -1)][random_spin_y]);
 
-    deltaE = 2*spins[random_spin_x][random_spin_y]*
-         (spins[random_spin_x][periodic(random_spin_y, N, 1)] + spins[random_spin_x][periodic(random_spin_y, N, -1)] +
-         spins[periodic(random_spin_x, N, 1)][random_spin_y] + spins[periodic(random_spin_x, N, -1)][random_spin_y]);
-
-    cout << "Spin to be flipped: " << random_spin_x << ", " << random_spin_y << endl;
-    cout << "DeltaE: " << deltaE << endl;
-    cout << "Compare " << random_number << " and " << w[deltaE+8] << endl;
-
-    if(random_number > w[deltaE+8]){
-         spins[random_spin_x][random_spin_y] *= -1;
-         E += deltaE;
-         M += 2*spins[random_spin_x][random_spin_y];
+            if(random_number >= w[deltaE+8]){
+                spins[random_spin_x][random_spin_y] *= -1;
+                E += deltaE;
+                M += 2*spins[random_spin_x][random_spin_y];
+            }
+        }
     }
 }
 
@@ -206,4 +193,29 @@ void Metropolis(int N, double random_number, int **spins, int& E, int& M, double
 double r2()
 {
     return (double)rand() / (double)RAND_MAX ;
+}
+
+
+void output(int N, int mc_cycles, double temperature, double* expectation_values)
+{
+double norm = 1.0/((double) (mc_cycles)); // divided by number of cycles
+double E_ExpectationValues = expectation_values[0]*norm;
+double E2_ExpectationValues = expectation_values[1]*norm;
+double M_ExpectationValues = expectation_values[2]*norm;
+double M2_ExpectationValues = expectation_values[3]*norm;
+double Mabs_ExpectationValues = expectation_values[4]*norm;
+
+// all expectation values are per spin, divide by 1/NSpins/NSpins
+
+double Evariance = (E2_ExpectationValues- E_ExpectationValues*E_ExpectationValues)/(N*N);
+double Mvariance = (M2_ExpectationValues - Mabs_ExpectationValues*Mabs_ExpectationValues)/(N*N);
+
+ofile << setiosflags(ios::showpoint | ios::uppercase);
+ofile << setw(15) << setprecision(8) << temperature;
+ofile << setw(15) << setprecision(8) << E_ExpectationValues/(N*N);
+ofile << setw(15) << setprecision(8) << Evariance/(temperature*temperature);
+ofile << setw(15) << setprecision(8) << M_ExpectationValues/(N*N);
+ofile << setw(15) << setprecision(8) << Mvariance/temperature;
+ofile << setw(15) << setprecision(8) << Mabs_ExpectationValues/(N*N);
+ofile << endl;
 }
